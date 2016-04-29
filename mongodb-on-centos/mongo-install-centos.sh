c7a0bbb5c8caee332432b01a3f7a1902ea38b5ab
Robert Klause <robklause@hotmail.com>
2016-04-29 08:58:05 -0400

    Mods for Enterprise install

--- mongodb-on-centos/mongo-install-centos.sh
+++ mongodb-on-centos/mongo-install-centos.sh
@@ -5,15 +5,16 @@
 # Enable write access to the mongodb.repo and configure it for installation
 
 #sudo chmod 777 /etc/yum.repos.d/mongodb.repo
-touch /etc/yum.repos.d/mongodb.repo
-echo "[mongodb-org-3.0]" >> /etc/yum.repos.d/mongodb.repo
-echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
-echo "baseurl=http://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.0/x86_64/" >> /etc/yum.repos.d/mongodb.repo
-echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb.repo
-echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
-
+touch /etc/yum.repos.d/mongodb-enterprise.repo 
+echo "[mongodb-org-3.0]" >> /etc/yum.repos.d/mongodb-enterprise.repo 
+echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb-enterprise.repo 
+echo "baseurl=https://repo.mongodb.com/yum/redhat/$releasever/mongodb-enterprise/stable/$basearch/" >> /etc/yum.repos.d/mongodb-enterprise.repo
+echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb-enterprise.repo
+echo "enabled=1" >> /etc/yum.repos.d/mongodb-enterprise.repo
+echo "gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc" >>  /etc/yum.repos.d/mongodb-enterprise.repo
 # Install updates
 yum -y update
 
-#Install Mongo DB
-yum install -y mongodb-org
+#Install Mongo DB & xfs driver
+yum install -y xfsprogs mongodb-org
+
